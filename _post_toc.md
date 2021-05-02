ef1e7ebfcb3deab056d4fdf70320782334529a9c
Dulguun Battulga <dolugen@gmail.com>
2021-05-02 17:00:21 +0200

    omit notebooks without title in toc

--- _post_toc.md
+++ _post_toc.md
@@ -8,6 +8,8 @@ pipenv run jupyter lab
 
 Invoke the `update-readme.sh` script. This will concatenate the header, footer and the generated TOC into the final `README.md`. That means `README.md` isn't edited directly.
 
+Only notebooks beginning with a markdown H1 title (e.g. `# title`) is listed in the TOC.
+
 - `_pre_toc.md` is the header file.
 - an untracked `_toc.md` file is created when `update-readme.sh` is called.
 - `_post_toc.md` is the footer file.
--- generate-toc.sh
+++ generate-toc.sh
@@ -5,15 +5,17 @@ nbviewer_badge="https://raw.githubusercontent.com/jupyter/design/master/logos/Ba
 function get_notebook_title() {
     local filepath;
     local first_line;
+    local first_cell_type;
     local title;
 
     filepath=$1;
+    first_cell_type=$(jq .cells[0].cell_type "$filepath");
     first_line=$(jq .cells[0].source[0] "$filepath");
-    if [[ "$first_line" =~ ^\"\#.* ]]; then
+    if [ "$first_cell_type" == '"markdown"' ] && [[ "$first_line" =~ ^\"\#.* ]]; then
         title=$(echo "$first_line" | sed 's/\"# //' | sed 's/\\n//' | sed 's/"$//');
         echo "$title"
     else
-        echo "_no title_"
+        return 1
     fi
 }
 
@@ -21,5 +23,7 @@ echo "| title | file | view notebook |"
 echo "|-------|------|---------------|"
 for filepath in $(fd ipynb); do
     title=$(get_notebook_title "$filepath");
-    echo "| $title | $filepath | [![nbviewer]($nbviewer_badge)](https://nbviewer.jupyter.org/dolugen/notebooks/blob/main/$filepath) |"
+    if [ $? != 1 ]; then
+        echo "| $title | \`$filepath\` | [![nbviewer]($nbviewer_badge)](https://nbviewer.jupyter.org/dolugen/notebooks/blob/main/$filepath) |";
+    fi
 done
